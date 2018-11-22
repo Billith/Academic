d119a94a6b7ba834033e52fc85844f0560628842
bilith <lukasdyduch@gmail.com>
2018-11-22 21:20:29 +0100

    New features

--- SYC/SpaceRobot/console.py
+++ SYC/SpaceRobot/console.py
@@ -4,37 +4,59 @@ import threading
 import SocketServer
 
 
+verbose = True
+
 class threaded_tcp_server(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     allow_reuse_address = True
     pass
 
 class tcp_command_handler(SocketServer.BaseRequestHandler):
     def handle(self):
-        command = self.request.recv(1024)
-        sys.stdout.write('\n[robot] %s ' % command)
+        command = self.request.recv(1024 * 1024)
+        if command == 'Przeszkoda! Procedura omijania wykonana' and not verbose:
+            return
+        print '\n[robot] %s ' % command
+
 
 def send_command(cmd, (ip,port)):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, 2323))
+    s.connect((ip,port))
     try:
         s.sendall(cmd)
-        print s.recv(1024)
+        print '[answer] %s' % s.recv(1024)
     finally:
         s.close()
 
 def start_listener():
+    global server
     server = threaded_tcp_server(('0.0.0.0', 2424), tcp_command_handler)
     server_thread = threading.Thread(target=server.serve_forever)
     server_thread.start()
     print '[*] Robot responses listener started'
 
 
-ip = raw_input('IP: ')
+ip = raw_input("[o] Enter robot's IP: ")
+start_listener()
 while True:
     cmd = raw_input('console # ')
     if cmd.lower() == 'quit' or cmd.lower() == 'exit':
         server.shutdown()
         sys.exit(0)
-    if not cmd:
+    elif not cmd:
         continue
+    elif cmd == 'verbose off':
+    	global verbose
+    	verbose = False
+    	continue
+    elif cmd == 'verbose on':
+    	global verbose
+    	verbose = True
+    	continue
+    elif cmd == 'back':
+    	pass
+    elif cmd == 'sendall':
+    	pass
+    else:
+    	print "[!] error: unknown command: '%s'" % cmd
+    	continue
     send_command(cmd, (ip,2323))
--- SYC/SpaceRobot/robot.py
+++ SYC/SpaceRobot/robot.py
@@ -3,6 +3,8 @@ import socket
 import io
 import threading
 import SocketServer
+import sys
+import json
 from datetime import datetime
 from random import randint
 from time import sleep
@@ -10,6 +12,8 @@ from time import sleep
 
 s = serial.serial_for_url('loop://', timeout=1)
 console_ip = ''
+storage = {}
+measurements = 0
 
 class threaded_tcp_server(SocketServer.ThreadingMixIn, SocketServer.TCPServer): 
     allow_reuse_address = True
@@ -18,29 +22,47 @@ class threaded_tcp_server(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 class tcp_command_handler(SocketServer.BaseRequestHandler):
     def handle(self):
     	global console_ip
+    	global storage
         if console_ip == '':
             console_ip = self.request.getpeername()[0]
         command = self.request.recv(1024)
         print '[cmd] %s ' % command
-        self.request.sendall('Ok!')
+        if command == 'sendall':
+            send_measurement(json.dumps(storage))
+        elif command == 'back':
+        	self.request.sendall('Ok! Comming back')
 
-def generate_reads(s):
+def generate_measurements(s):
     sio = io.TextIOWrapper(io.BufferedRWPair(s,s))
     print '[*] Starts generating data'
     while True:
         # format danych: czas|oswietlenie|temperatura|cisnienie|przeszkoda
-        sio.write(unicode('%s|%i|%i|%i|%i\n' % (str(datetime.now()), randint(450,700), randint(15,25), randint(950,1100), randint(0,1))))
+        sio.write(unicode('%s|%i|%i|%i|%i\n' % (
+        	str(datetime.now()), 
+        	randint(450,700), 
+        	randint(15,25), 
+        	randint(950,1100), 
+        	randint(0,1)))
+        )
         sio.flush()
         sleep(1)
 
 def parse_data(data):
-    time,lighting_level,temp,pressure,obstacle = data.split('|')
-    # Debug
-    #print 'czas: %s\npoziom oswietlenia: %s\ntemperatura: %s\ncisnienie: %s\nprzeszkoda: %s' % (time,lighting_level,temp,pressure,obstacle)
+    global measurements
+    global storage
+    measurements += 1
+    time,lighting_level,temp,pressure,obstacle = data.strip().split('|')
+    store_data(time,lighting_level,temp,pressure,obstacle)
     if obstacle.strip() == unicode('1'):
         send_obstacle_info()
+    if measurements % 10 == 0:
+    	send_measurement(storage[measurements])
+    	print '[*] Measurement sent!'
 
 def send_obstacle_info():
+	# przesylanie informacji nie zacznie sie dopoki z konsoli nie 
+	# zostanie wyslany dowolny, komunikat (przypisanie IP konsoli)
+    global console_ip
     if console_ip == '':
         return
     try:
@@ -48,12 +70,33 @@ def send_obstacle_info():
         s.connect((console_ip,2424))
         s.sendall('Przeszkoda! Procedura omijania wykonana')
     except:
-        pass
+        print '[!] Failed to send information about obstacle'
+        print sys.exc_info()[0]
     finally:
         s.close()
 
-def store_data():
-    pass
+def send_measurement(data):
+    global console_ip
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((console_ip,2424))
+        s.sendall(json.dumps(data))
+    except:
+        print '[!] Failed to send measurement'
+        print sys.exc_info()[0]
+    finally:
+        s.close()
+
+def store_data(time,lighting_level,temp,pressure,obstacle):
+    global storage
+    global measurements
+    storage[measurements] = {
+    	'time'			 : time,
+    	'lighting_level' : lighting_level,
+    	'temp' 			 : temp,
+    	'pressure'		 : pressure,
+    	'obstacle'		 : obstacle,
+    }
 
 def start_listener():
     server = threaded_tcp_server(('0.0.0.0', 2323), tcp_command_handler)
@@ -62,7 +105,7 @@ def start_listener():
     print '[*] Command listener started'
 
 
-generating_thread = threading.Thread(target=generate_reads, args=(s,))
+generating_thread = threading.Thread(target=generate_measurements, args=(s,))
 generating_thread.start()
 start_listener()
 
