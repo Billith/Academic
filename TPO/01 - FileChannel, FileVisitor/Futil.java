e6388a74b4f9672022dde31f93ad4f45f887d6eb
bilith <lukasdyduch@gmail.com>
2019-03-11 21:59:44 +0100

    Slightly changes naming

--- TPO/01 - FileChannel, FileVisitor/Futil.java
+++ TPO/01 - FileChannel, FileVisitor/Futil.java
@@ -11,28 +11,28 @@ import java.nio.file.attribute.BasicFileAttributes;
 public class Futil {
     public static void processDir(String dirName, String resultFileName) {
         try {
-            OpenOption[] options = new OpenOption[] { StandardOpenOption.CREATE, StandardOpenOption.WRITE };
-            FileChannel target = FileChannel.open(Paths.get(resultFileName), options);
-            Files.walkFileTree(Paths.get(dirName), new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    FileChannel source = FileChannel.open(file);
-                    ByteBuffer buffer = ByteBuffer.allocate(8192);
-                    int read_bytes;
-                    while ((read_bytes = source.read(buffer)) != -1) {
-                        byte[] bufferBytesCopy = new byte[read_bytes];
-                        for (int i=0; i<read_bytes;i++) {
-                            bufferBytesCopy[i] = buffer.get(i);
-                        }
-                        CharBuffer encodedFilesContent = Charset.forName("cp1250").decode(ByteBuffer.wrap(bufferBytesCopy));
-                        ByteBuffer decodedFilesContent = Charset.forName("utf-8").encode(encodedFilesContent);
-                        target.write(decodedFilesContent);
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			OpenOption[] options = new OpenOption[] { StandardOpenOption.CREATE, StandardOpenOption.WRITE };
+        	FileChannel target = FileChannel.open(Paths.get(resultFileName), options);
+			Files.walkFileTree(Paths.get(dirName), new SimpleFileVisitor<Path>() {
+			    @Override
+			    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+			        FileChannel source = FileChannel.open(file);
+			        ByteBuffer buffer = ByteBuffer.allocate(8192);
+			        int read_bytes;
+			        while ((read_bytes = source.read(buffer)) != -1) {
+			        	byte[] copyOfBuffersBytesArray = new byte[read_bytes];
+			        	for (int i=0; i < read_bytes; i++) {
+							copyOfBuffersBytesArray[i] = buffer.get(i);
+						}
+						CharBuffer encodedContentOfFiles = Charset.forName("cp1250").decode(ByteBuffer.wrap(copyOfBuffersBytesArray));
+			        	ByteBuffer decodedContentOfFiles = Charset.forName("utf-8").encode(encodedContentOfFiles);
+			            target.write(decodedContentOfFiles);
+			        }
+			        return FileVisitResult.CONTINUE;
+			    }
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
