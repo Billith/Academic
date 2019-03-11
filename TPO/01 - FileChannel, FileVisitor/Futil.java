0527bc42da03d1b6c18c5e8c8b6609b84900ebe9
bilith <lukasdyduch@gmail.com>
2019-03-11 21:11:10 +0100

    Adds usage of required classes in TPO1

--- TPO/01 - FileChannel, FileVisitor/Futil.java
+++ TPO/01 - FileChannel, FileVisitor/Futil.java
@@ -1,8 +1,8 @@
 package zad1;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.nio.file.*;
@@ -10,22 +10,23 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 public class Futil {
     public static void processDir(String dirName, String resultFileName) {
-        Path dirNamePath = Paths.get(dirName);
         try {
-            FileChannel target = new FileOutputStream(resultFileName).getChannel();
-            Files.walkFileTree(dirNamePath, new SimpleFileVisitor<Path>() {
+            OpenOption[] options = new OpenOption[] { StandardOpenOption.CREATE, StandardOpenOption.WRITE };
+            FileChannel target = FileChannel.open(Paths.get(resultFileName), options);
+            Files.walkFileTree(Paths.get(dirName), new SimpleFileVisitor<Path>() {
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                     FileChannel source = FileChannel.open(file);
                     ByteBuffer buffer = ByteBuffer.allocate(8192);
                     int read_bytes;
                     while ((read_bytes = source.read(buffer)) != -1) {
-                        //String content = new String(buffer.array(), 0, read_bytes, "cp1250");
-                        ByteBuffer content = Charset.forName("utf-8").encode(
-                                new String(buffer.array(), 0, read_bytes, "cp1250")
-                        );
-                        target.write(content);
-                        //target.write(ByteBuffer.wrap(content.getBytes(StandardCharsets.UTF_8)));
+                        byte[] bufferBytesCopy = new byte[read_bytes];
+                        for (int i=0; i<read_bytes;i++) {
+                            bufferBytesCopy[i] = buffer.get(i);
+                        }
+                        CharBuffer encodedFilesContent = Charset.forName("cp1250").decode(ByteBuffer.wrap(bufferBytesCopy));
+                        ByteBuffer decodedFilesContent = Charset.forName("utf-8").encode(encodedFilesContent);
+                        target.write(decodedFilesContent);
                     }
                     return FileVisitResult.CONTINUE;
                 }
