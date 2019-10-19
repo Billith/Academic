902caae376b01265d9dbbbd60388df8ad7c605c9
Billith <lukasdyduch@gmail.com>
2019-10-19 11:25:26 +0200

    MIW project 1

--- MIW/Projekt1/02c_regresja.py
+++ MIW/Projekt1/02c_regresja.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 
 
 a = np.loadtxt('dane7.txt')
+tren, test = np.array_split(a, 2)
 
-x = a[:,[1]]
-y = a[:,[0]]
+x = tren[:,[0]]
+y = tren[:,[1]]
 
 c = np.hstack([x*x*x, x*x, x, np.ones(x.shape)])
 v = np.linalg.pinv(c) @ y
@@ -13,12 +14,7 @@ cc = np.hstack([1/x, np.ones(x.shape)])
 vv = np.linalg.pinv(cc) @ y
 
 plt.plot(x, y, 'ro')
-plt.plot(x,v[0]*x*x*x + v[1]*x*x + v[2]*x + v[3],)
-plt.plot(x,vv[0]/x + vv[1])
+plt.plot(x, v[0]*x*x*x + v[1]*x*x + v[2]*x + v[3])
+plt.plot(x, vv[0]/x + vv[1])
 plt.show()
-
-c1 = np.hstack([x*x*x, x*x, x, np.ones(x.shape)])
-v1 = np.linalg.pinv(c) @ y
-cc1 = np.hstack([1/x, np.ones(x.shape)])
-cc1 = np.linalg.pinv(c1) @ y
 
