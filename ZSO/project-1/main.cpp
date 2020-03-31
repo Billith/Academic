2bf64de82a5b9b505f025fdaf0c4633665610c1a
bilith <lukasdyduch@gmail.com>
2020-03-31 23:25:35 +0200

    ZSO projekt 1

--- ZSO/project-1/main.cpp
+++ ZSO/project-1/main.cpp
@@ -5,29 +5,39 @@
 
 #define DEBUG
 #define DELAY
+#define COUNT 1
 
 
 std::mutex g_lock;
 
-void dummy(int count) {
-    for(int i=0; i<count; i++) {
-        g_lock.lock();
+void child(int counter) {
+    g_lock.lock();
+    
+    for (int i=0; i < counter; i++) {
         #ifdef DEBUG
         std::cout << "Thread id: " << std::this_thread::get_id() << std::endl;
         #endif
+
         g_lock.unlock();
+
         #ifdef DELAY
         std::this_thread::sleep_for(std::chrono::seconds(1));
         #endif
     }
 }
 
-int main(int argc, char* argv[]) {
-    std::thread t1(dummy, 5);
-    std::thread t2(dummy, 5);
+void parent() {
+    std::thread c1(child, COUNT);
+    std::thread c2(child, COUNT);
+    std::thread c3(child, COUNT);
+    c1.detach();
+    c2.detach();
+    c3.detach();
+}
 
-    t1.join();
-    t2.join();
-
-    std::cout << "All threads has finished" << std::endl;
+int main() {
+    for (int i=0; i < 10; i++) {
+        std::thread p1(parent);
+        p1.join();
+    }
 }
