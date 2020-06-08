d96b95fadbfe9d6dd6a2ae145d0b983014b8305e
bilith <lukasdyduch@gmail.com>
2020-06-08 19:27:22 +0200

    Update generate_parser.sh

--- AUG/projekt/generate_parser.sh
+++ AUG/projekt/generate_parser.sh
@@ -1,8 +1,5 @@
 #!/bin/bash +x
 
 cd grammar
-<<<<<<< HEAD
 java -jar /root/antlr-4.8-complete.jar -Dlanguage=Python3 HttpLang.g4 -o ../gen
-=======
 antlr4 -Dlanguage=Python3 HttpLang.g4 -o ../gen
->>>>>>> 75981e43ac6b528b2ff1f440faab7fe9f5cd5641
